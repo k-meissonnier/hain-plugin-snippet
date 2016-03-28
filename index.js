61b6364d91451c42b8cb03ee7597bd34578f65f0
Kevin <kevin.meissonnier@outlook.com>
2016-03-28 23:48:01 +0200

    Update index.js / gitignore/ package.json

--- index.js
+++ index.js
@@ -1,27 +1,102 @@
 'use strict';
 
 module.exports = (pluginContext) => {
-    const shell = pluginContext.shell;
+    var logger = pluginContext.logger;
+    var app = pluginContext.app;
 
+    var fs = require('fs');
+    var LineByLineReader = require('line-by-line');
+    var ncp = require("copy-paste");
+
+    const dir = '../../hain-snippet-plugin';
+    const file = dir + "/snippet.txt";
+
+
+        
     function search(query, res) {
         const query_trim = query.trim();
+       
         if (query_trim.length == 0) {
             return;
         }
+
         res.add({
             id: query_trim,
-            payload: 'open',
-            title: 'Search ' + query_trim,
-            desc: 'Search in Laravel API'
+            payload: 'write',
+            title: `<b>Add : ${query_trim}</b>`,
+            desc: 'Save this snippet',
+            icon: '#fa fa-plus'
         });
+
+        var lr = new LineByLineReader(file);
+
+        lr.on('line', function (line) {
+            if(line != '' && line.indexOf(query_trim) > -1) {
+                var lineWithBold = line.replace(query_trim, '<b>' + query_trim + '</b>');
+                res.add({
+                    id: line,
+                    payload: 'copy',
+                    title: lineWithBold,
+                    desc: "Click... It's Copied!",
+                    icon: '#fa fa-files-o'
+                });
+            }
+        });
+
     }
 
+  
     function execute(id, payload) {
-        if (payload !== 'open') {
-            return;
+
+        if (payload == 'write') {
+
+            if (!fs.existsSync(dir)){
+                fs.mkdirSync(dir);
+            }
+
+            if (fs.existsSync(file)) {
+
+                var lr = new LineByLineReader(file);
+                var exist = false;
+
+                lr.on('line', function (line) {
+                    if (line == id) {
+                        exist = true;
+                    }
+                });
+
+                lr.on('end', function () {
+                    if( !exist ){
+                        write(id);
+                    }
+                }); 
+
+            } 
+
+            else {
+                write(id);
+            }
+
+            app.setInput('/snippet ');
         }
-        shell.openExternal(`https://laravel.com/api/5.2/search.html?search=${id}`);
+
+        if (payload == 'copy') {
+            ncp.copy(id, function(){
+                app.close();
+            });
+        }
+
     }
+
+
+    function write(value) {
+        fs.appendFile(file, '\n' + value, function(err) {
+            if(err) {
+                logger.log(err);
+            }
+        }); 
+    }
+
 
     return {search, execute};
 };
